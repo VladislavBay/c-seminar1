4a0e86fd06529dc02931c3cdd19ae335c428fdd9
vladislav <bayanovlad@gmail.com>
2023-09-24 16:07:24 +0300

    math2

--- Program.cs
+++ Program.cs
@@ -1,5 +1,20 @@
 ﻿// 
+// Console.WriteLine("Hello, press your number");
+// int num = Convert.ToInt32(Console.ReadLine());
+// int num2 = num * num;
+// Console.WriteLine(num2);
+
 Console.WriteLine("Hello, press your number");
-int num = Convert.ToInt32(Console.ReadLine());
-int num2 = num * num;
-Console.WriteLine(num2);
+int num1 = Convert.ToInt32(Console.ReadLine());
+ 
+Console.WriteLine("Hello, press your number");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+if (num1 == num2*num2)
+{
+    Console.WriteLine("yes");
+}
+else
+{
+    Console.WriteLine("no");
+}
