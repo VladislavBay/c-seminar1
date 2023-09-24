caf80467c2a89d5375a0cc1329ae7bd28ec241ab
vladislav <bayanovlad@gmail.com>
2023-09-24 16:14:15 +0300

    math3

--- Program.cs
+++ Program.cs
@@ -4,17 +4,48 @@
 // int num2 = num * num;
 // Console.WriteLine(num2);
 
+// Console.WriteLine("Hello, press your number");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+ 
+// Console.WriteLine("Hello, press your number");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+
+// if (num1 == num2*num2)
+// {
+//     Console.WriteLine("yes");
+// }
+// else
+// {
+//     Console.WriteLine("no");
+// }
 Console.WriteLine("Hello, press your number");
 int num1 = Convert.ToInt32(Console.ReadLine());
- 
-Console.WriteLine("Hello, press your number");
-int num2 = Convert.ToInt32(Console.ReadLine());
 
-if (num1 == num2*num2)
+if (num1 == 1)
 {
-    Console.WriteLine("yes");
+    Console.WriteLine("понедедльник");
 }
-else
+if (num1 == 2)
 {
-    Console.WriteLine("no");
+    Console.WriteLine("вторник");
+}
+if (num1 == 3)
+{
+    Console.WriteLine("среда");
+}
+if (num1 == 4)
+{
+    Console.WriteLine("четверг");
+}
+if (num1 == 5)
+{
+    Console.WriteLine("пятница");
+}
+if (num1 == 6)
+{
+    Console.WriteLine("суббота");
+}
+if (num1 == 7)
+{
+    Console.WriteLine("воскресенье");
 }
